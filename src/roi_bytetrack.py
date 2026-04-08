cefaf2cfc8ceb7c94bb2e8867082fab32c14ea16
lalkaman1981 <oleg.shtogrin2005@gmail.com>
2026-04-08 20:49:37 +0300

    update tracking

--- src/roi_bytetrack.py
+++ src/roi_bytetrack.py
@@ -13,7 +13,11 @@ from torchvision.models import resnet50, ResNet50_Weights
 
 from supervision.detection.core import Detections
 from supervision.detection.utils.iou_and_nms import box_iou_batch
+from supervision.tracker.byte_tracker.single_object_track import TrackState
 from supervision.tracker.byte_tracker import matching
+from supervision.tracker.byte_tracker.core import joint_tracks, sub_tracks, remove_duplicate_tracks
+
+from strack import STrack
 
 
 class ROIByteTrack:
@@ -45,7 +49,204 @@ class ROIByteTrack:
             minimum_matching_threshold=0.8,
             frame_rate=30
         )
+        
+        self.nigger = 0
+    
+    @staticmethod
+    def embedding_distance(strack_pool, detections): #TODO: make not static
 
+        strack_pool_len = len(strack_pool)
+        detections_len = len(detections)
+
+        if strack_pool_len == 0 or detections_len == 0:
+            return np.zeros((strack_pool_len, detections_len), dtype=np.float32)
+
+        #device = "cuda" if torch.cuda.is_available() else "cpu"
+        
+        pool_feats = torch.stack([
+            torch.as_tensor(t.curr_feat, dtype=torch.float32)#, device=device) 
+            for t in strack_pool
+        ])
+        
+        det_feats = torch.stack([
+            torch.as_tensor(d.curr_feat, dtype=torch.float32)#, device=device) 
+            for d in detections
+        ])
+
+        cos_sim = torch.cosine_similarity(
+            pool_feats.unsqueeze(1),
+            det_feats.unsqueeze(0),
+            dim=2
+        )
+
+        cost_matrix = 1 - cos_sim
+
+        return cost_matrix.cpu().numpy()
+        
+    @staticmethod
+    def update_with_tensors(self, tensors: np.ndarray, embeddings: np.ndarray = None, roi_coef = 0.5) -> list[STrack]:
+
+        self.frame_id += 1
+        activated_starcks = []
+        refind_stracks = []
+        lost_stracks = []
+        removed_stracks = []
+
+        scores = tensors[:, 4]
+        bboxes = tensors[:, :4]
+
+        remain_inds = scores > self.track_activation_threshold
+        inds_low = scores > 0.1
+        inds_high = scores < self.track_activation_threshold
+
+        inds_second = np.logical_and(inds_low, inds_high)
+        dets_second = bboxes[inds_second]
+        dets = bboxes[remain_inds]
+        scores_keep = scores[remain_inds]
+        scores_second = scores[inds_second]
+
+        if len(dets) > 0:
+            """Detections"""
+            detections = [
+                STrack(
+                    STrack.tlbr_to_tlwh(tlbr),
+                    score_keep,
+                    self.minimum_consecutive_frames,
+                    self.shared_kalman,
+                    self.internal_id_counter,
+                    self.external_id_counter,
+                    curr_feat=embeddings[remain_inds][i] if embeddings is not None else None #my change is here
+                )
+                for i, (tlbr, score_keep) in enumerate(zip(dets, scores_keep))
+            ]
+        else:
+            detections = []
+
+        """ Add newly detected tracklets to tracked_stracks"""
+        unconfirmed = []
+        tracked_stracks = []  # type: list[STrack]
+
+        for track in self.tracked_tracks:
+            if not track.is_activated:
+                unconfirmed.append(track)
+            else:
+                tracked_stracks.append(track)
+
+        """ Step 2: First association, with high score detection boxes"""
+        strack_pool = joint_tracks(tracked_stracks, self.lost_tracks)
+
+        # Predict the current location with KF
+        STrack.multi_predict(strack_pool, self.shared_kalman)
+        iou_dists = matching.iou_distance(strack_pool, detections)
+
+        if embeddings is not None:
+            emb_dists = ROIByteTrack.embedding_distance(strack_pool, detections) 
+  
+            dists = (1 - roi_coef) * iou_dists + roi_coef * emb_dists
+        else:
+            dists = iou_dists
+
+        dists = matching.fuse_score(dists, detections)
+        matches, u_track, u_detection = matching.linear_assignment(
+            dists, thresh=self.minimum_matching_threshold
+        )
+
+        for itracked, idet in matches:
+            track = strack_pool[itracked]
+            det = detections[idet]
+            if track.state == TrackState.Tracked:
+                track.update(detections[idet], self.frame_id)
+                activated_starcks.append(track)
+            else:
+                track.re_activate(det, self.frame_id)
+                refind_stracks.append(track)
+
+        """ Step 3: Second association, with low score detection boxes"""
+        # association the untrack to the low score detections
+        if len(dets_second) > 0:
+            """Detections"""
+            detections_second = [
+                STrack(
+                    STrack.tlbr_to_tlwh(tlbr),
+                    score_second,
+                    self.minimum_consecutive_frames,
+                    self.shared_kalman,
+                    self.internal_id_counter,
+                    self.external_id_counter,
+                )
+                for (tlbr, score_second) in zip(dets_second, scores_second)
+            ]
+        else:
+            detections_second = []
+        r_tracked_stracks = [
+            strack_pool[i]
+            for i in u_track
+            if strack_pool[i].state == TrackState.Tracked
+        ]
+        dists = matching.iou_distance(r_tracked_stracks, detections_second)
+        matches, u_track, u_detection_second = matching.linear_assignment(
+            dists, thresh=0.5
+        )
+        for itracked, idet in matches:
+            track = r_tracked_stracks[itracked]
+            det = detections_second[idet]
+            if track.state == TrackState.Tracked:
+                track.update(det, self.frame_id)
+                activated_starcks.append(track)
+            else:
+                track.re_activate(det, self.frame_id)
+                refind_stracks.append(track)
+
+        for it in u_track:
+            track = r_tracked_stracks[it]
+            if not track.state == TrackState.Lost:
+                track.state = TrackState.Lost
+                lost_stracks.append(track)
+
+        """Deal with unconfirmed tracks, usually tracks with only one beginning frame"""
+        detections = [detections[i] for i in u_detection]
+        dists = matching.iou_distance(unconfirmed, detections)
+
+        dists = matching.fuse_score(dists, detections)
+        matches, u_unconfirmed, u_detection = matching.linear_assignment(
+            dists, thresh=0.7
+        )
+        for itracked, idet in matches:
+            unconfirmed[itracked].update(detections[idet], self.frame_id)
+            activated_starcks.append(unconfirmed[itracked])
+        for it in u_unconfirmed:
+            track = unconfirmed[it]
+            track.state = TrackState.Removed
+            removed_stracks.append(track)
+
+        """ Step 4: Init new stracks"""
+        for inew in u_detection:
+            track = detections[inew]
+            if track.score < self.det_thresh:
+                continue
+            track.activate(self.kalman_filter, self.frame_id)
+            activated_starcks.append(track)
+        """ Step 5: Update state"""
+        for track in self.lost_tracks:
+            if self.frame_id - track.frame_id > self.max_time_lost:
+                track.state = TrackState.Removed
+                removed_stracks.append(track)
+
+        self.tracked_tracks = [
+            t for t in self.tracked_tracks if t.state == TrackState.Tracked
+        ]
+        self.tracked_tracks = joint_tracks(self.tracked_tracks, activated_starcks)
+        self.tracked_tracks = joint_tracks(self.tracked_tracks, refind_stracks)
+        self.lost_tracks = sub_tracks(self.lost_tracks, self.tracked_tracks)
+        self.lost_tracks.extend(lost_stracks)
+        self.lost_tracks = sub_tracks(self.lost_tracks, self.removed_tracks)
+        self.removed_tracks = removed_stracks
+        self.tracked_tracks, self.lost_tracks = remove_duplicate_tracks(
+            self.tracked_tracks, self.lost_tracks
+        )
+        output_stracks = [track for track in self.tracked_tracks if track.is_activated]
+
+        return output_stracks
 
     def extract_embeddings(self, frame, boxes):
         """
@@ -56,7 +257,7 @@ class ROIByteTrack:
             return torch.empty((0, 2048)).to(self.device)
 
         rois = torch.zeros((len(boxes), 5)).to(self.device)
-        rois[:, 1:] = torch.tensor(boxes).to(self.device)
+        rois[:, 1:] = boxes
         
         crops = []
         for box in boxes:
@@ -77,7 +278,7 @@ class ROIByteTrack:
 
         return embeddings
 
-    def update_with_detections_roi(self, detections: Detections, frame = None, roi_coef = 0.5) -> Detections:
+    def update_with_detections_roi(self, detections: Detections, embeddings = None, roi_coef = 0.5) -> Detections:
         """
         Took this from supervision library
         """
@@ -87,26 +288,16 @@ class ROIByteTrack:
                 detections.confidence[:, np.newaxis],
             )
         )
-        tracks = self.tracker.update_with_tensors(tensors=tensors)
+        tracks = ROIByteTrack.update_with_tensors(self.tracker,
+                                                  tensors=tensors,
+                                                  embeddings=embeddings,
+                                                  roi_coef = roi_coef)
 
         if len(tracks) > 0:
             detection_bounding_boxes = np.asarray([det[:4] for det in tensors])
             track_bounding_boxes = np.asarray([track.tlbr for track in tracks])
 
             ious = box_iou_batch(detection_bounding_boxes, track_bounding_boxes)
-
-            if frame is not None:
-                
-                detection_embeddings = self.extract_embeddings(frame, detection_bounding_boxes)
-                track_embeddings = self.extract_embeddings(frame, track_bounding_boxes)
-
-                cos_sim = torch.cosine_similarity(
-                    detection_embeddings.unsqueeze(1),  # (21, 1, D)
-                    track_embeddings.unsqueeze(0),      # (1, 20, D)
-                    dim=2
-                ).cpu().numpy()
-
-                ious = (1 - roi_coef) * ious + roi_coef * cos_sim
 
             iou_costs = 1 - ious
 
@@ -152,7 +343,11 @@ class ROIByteTrack:
                 detections = sv.Detections.from_ultralytics(results)
 
                 if use_roi:
-                    detections = self.update_with_detections_roi(detections, frame, roi_coef)
+                    boxes = results.boxes.data[:, :4]
+                    
+                    embeddings = self.extract_embeddings(frame, boxes)
+                    
+                    detections = self.update_with_detections_roi(detections, embeddings, roi_coef)
                 else:
                     detections = self.update_with_detections_roi(detections)
 
@@ -208,7 +403,7 @@ class ROIByteTrack:
         mh = mm.metrics.create()
 
         metrics_list = ['mota', 'motp', 'idf1', 'mostly_tracked', 'mostly_lost', 'num_switches']
-        
+
         summary = mh.compute(acc, metrics=metrics_list, name='ByteTrack_Baseline')
         
         if verbose:
@@ -221,6 +416,7 @@ class ROIByteTrack:
             print(str_summary)
 
         return summary
+
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -241,14 +437,14 @@ def main():
     
     mot_metricks_path = "detection_metrics.txt" # path where mot metricks will be stored, maybe will rework this
     
-    track.process_traching("../task1/VisDrone2019-MOT-test-dev/sequences/uav0000009_03358_v", # path to dataset image sequence
+    track.process_traching("../task1_2/VisDrone2019-MOT-test-dev/sequences/uav0000009_03358_v", # path to dataset image sequence
                            "output_images_tracked",  #p ath to output tracked objects on the image
                            mot_metricks_path,
                            use_roi = True,
-                           roi_coef = 0.3
+                           roi_coef = 0.2
                            )
-    
-    track.evaluate_mot("../task1/VisDrone2019-MOT-test-dev/annotations/uav0000009_03358_v.txt", #path to annotations
+
+    track.evaluate_mot("../task1_2/VisDrone2019-MOT-test-dev/annotations/uav0000009_03358_v.txt", #path to annotations
                        mot_metricks_path,
                        verbose = True
                        )
